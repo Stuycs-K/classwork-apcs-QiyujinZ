a95fdb3f535e91933c564993e22e66823643fdfe
Qiyujin Zhang <qzhan7183@gmail.com>
2024-10-01 00:35:08 -0400

    2nd ed. added methods and added more test cases. should be the final version.

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -21,23 +21,27 @@ public class ArrayMethods{
       */
     public static String arrToString(int[][]ary){
       //this should use arrToString(int[])
-      
-      String ans = "[";
-      
+      String newAry = "[";
       for (int i = 0; i < ary.length-1; i++){
-        ans += aryToString(ary[i]) + ", ";
+        newAry += aryToString(ary[i]) + ", ";
       }
       if (ary.length != 0){
-        ans += ary[ary.length-1];       
+        newAry += aryToString(ary[ary.length-1]);
       }
-      ary += "]";
-      return ans;
+      newAry += "]";
+      return newAry;
     }
 
     /*Return the sum of all of the values in the 2D array */
     public static int arr2DSum(int[][]nums){
       //use a nested loop to solve this
-      int[][] ans = new int[][]{}
+      int sum = 0;
+      for (int i = 0; i < nums.length; i ++){
+        for (int j = 0; j < nums[i].length; j ++){
+          sum += nums[i][j];
+        }
+      }
+      return sum;
     }
 
     /**Rotate an array by returning a new array with the rows and columns swapped.
@@ -45,11 +49,20 @@ public class ArrayMethods{
       * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
       */
     public static int[][] swapRC(int[][]nums){
-      return new int[1][1];
+      int[][] newNums = new int[nums[0].length][nums.length];
+      for (int i = 0; i < nums[0].length; i ++){
+        for (int j = 0; j < nums.length; j ++){
+          newNums[i][j] = nums[j][i];
+        }
+      }
+      return newNums;
     }
     
     public static void main (String[] args){
-    
+      int[][] arry1 = new int[][]{{0,1,2,3},{4,5,6,7},{8,9,10,11}};
+      System.out.println(arrToString(arry1));
+      System.out.println(arr2DSum(arry1));
+      System.out.println(arrToString(swapRC(arry1)));
   }
 }
 
