4d02d844246414f1eefee4c88a6cc05a922f4367
Qiyujin Zhang <qzhan7183@gmail.com>
2024-10-02 23:13:28 -0400

    4th ed. Added replaceNegative and copy functions and respective test cases. Ran properly.

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -50,9 +50,9 @@ public class ArrayMethods{
       */
     public static int[][] swapRC(int[][]nums){
       int[][] newNums = new int[nums[0].length][nums.length];
-      for (int i = 0; i < nums[0].length; i ++){
-        for (int j = 0; j < nums.length; j ++){
-          newNums[i][j] = nums[j][i];
+      for (int i = 0; i < nums.length; i ++){
+        for (int j = 0; j < nums[i].length; j ++){
+          newNums[j][i] = nums[i][j];
         }
       }
       return newNums;
@@ -62,9 +62,19 @@ public class ArrayMethods{
     //-When the row number is the same as the column number replace
     //that negative with the value 1
     //-All other negatives replace with 0
-    public static void replaceNegative(int[][] vals){ 
-      
-    }
+   public static void replaceNegative(int[][] vals){
+        for( int i =0; i< vals.length; i++){
+          for (int j = 0; j < vals[i].length; j++){
+            if(vals[i][j] < 0 && i == j){
+              vals[i][j] = 1;
+            }
+            if(vals[i][j] < 0 && i != j){
+              vals[i][j] = 0;
+            }
+          }
+        }
+        System.out.println(arrToString(vals));
+   }
 
     //4. Make a copy of the given 2d array.
     //When testing : make sure that changing the original does NOT change the copy.
@@ -72,14 +82,71 @@ public class ArrayMethods{
     //You SHOULD write a helper method for this.
     //If you don't see a good way to do that, you should stop and look at prior methods.
     public static int[][] copy(int[][] nums){
-      return null;//placeholder so it compiles
+      int [][] newNums = create2DArray(nums);
+      int temp = 0;
+      for (int i  = 0; i < nums.length; i ++){
+        for (int j  = 0; j < nums[i].length; j ++){
+          temp = nums[i][j];
+          newNums[i][j] = temp;
+        }
+      }
+      return newNums;
     }
-    
+
+    public static int[][] create2DArray(int[][] nums){
+      int[][] newNums = new int[nums.length][];
+      for (int i = 0; i < nums.length; i ++){
+        newNums[i] = new int[nums[i].length];
+      }
+      return newNums;
+  }
+
+    public static void copyTest (int[][] nums){
+      int[][] newNums = copy(nums);
+      System.out.println(arrToString(newNums));
+      replaceNegative(nums);
+      System.out.println(arrToString(newNums));
+}
+
     public static void main (String[] args){
+        int[][] empty2D = new int[3][0]; // empty rows
+        int[][] rectangular2D = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}; // [4][3]
+        int[][] square2D = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // [3][3]
+        int[][] ragged2D = {{1, 2, 3, 4, 5, 6}, {7}, {8, 9}, {10, 11, 12, 13}}; // ragged with varying lengths
+        int[][] negative2D = {{-1, -25, 25, 1}, {23, 34, -100, 0, -3}};
       int[][] arry1 = new int[][]{{0,1,2,3},{4,5,6,7},{8,9,10,11}};
       System.out.println(arrToString(arry1));
       System.out.println(arr2DSum(arry1));
       System.out.println(arrToString(swapRC(arry1)));
+        // arrToString tests with 2D arrays
+        System.out.println("String tests: ");
+        System.out.println(arrToString(empty2D));
+        System.out.println(arrToString(rectangular2D));
+        System.out.println(arrToString(square2D));
+        System.out.println(arrToString(ragged2D));
+
+        // 2D sum tests
+        System.out.println("Sum tests: ");
+        System.out.println(arr2DSum(empty2D));
+        System.out.println(arr2DSum(rectangular2D));
+        System.out.println(arr2DSum(square2D));
+        System.out.println(arr2DSum(ragged2D));
+
+        // swapRC tests
+        System.out.println("swapRC tests: ");
+        System.out.println(arrToString(swapRC(empty2D)));
+        System.out.println(arrToString(swapRC(rectangular2D)));
+        System.out.println(arrToString(swapRC(square2D)));
+        System.out.println(arrToString(swapRC(ragged2D))); // will create some arrays with 0 as other indexes because first array is larger.
+
+
+        // copy array tests
+        System.out.println("copy tests: ");
+        copyTest(negative2D);
+
+        // replace negative tests
+        System.out.println("replaceNegative tests: ");
+        replaceNegative(negative2D);
+        replaceNegative(empty2D);
   }
 }
-
