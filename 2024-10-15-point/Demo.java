2ac38ef65d1e9fb990a965664b8a622974407d3f
Qiyujin Zhang <qzhan7183@gmail.com>
2024-10-15 11:39:26 -0400

    2nd ed. Added distance function and new test cases for toString method.

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,16 +5,20 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return 0.0;
+    return Math.sqrt(Math.pow((a.getX()-b.getX()), 2)+Math.pow((a.getY()-b.getY()), 2));
   }
 
   public static void main(String[]args){
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
-    System.out.println( p3);
-    System.out.println( distance(p1,p2));
-    System.out.println( Point.distance(p1,p2));
-    System.out.println( p1.distanceTo(p2));
+    System.out.println(p3);
+    System.out.println(p3.toString());
+    Point p4 = new Point(p3);
+    System.out.println(p4);
+    
+    System.out.println(distance(p1,p2));
+    System.out.println(Point.distance(p1,p2));
+    System.out.println(p1.distanceTo(p2));
   }
 }
