a53bf30fb846ee45b28d6336adc85fd3dc5f7437
Qiyujin Zhang <qzhan7183@gmail.com>
2024-10-15 11:45:59 -0400

    2nd ed. Added Point and distanceTo functions

--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -7,8 +7,8 @@ public class Point{
 
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
-    //COMPLETE THIS SECOND
-    //Then write test cases.
+    this.x=other.getX();
+    this.y=other.getY();
   }
 
   //Initialize this Point to have the provided values
@@ -27,7 +27,7 @@ public class Point{
   Write this method FOURTH, go to the main and do that one first.
   */
   public double distanceTo(Point other){
-    return 0.0;
+    return Math.sqrt(Math.pow((x-other.getX()), 2)+Math.pow((y-other.getY()), 2));
   }
 
   /*
@@ -35,7 +35,7 @@ public class Point{
   do not write redundant code.
   */
   public static double distance(Point a, Point b){
-    return 0.0;
+    return Math.sqrt(Math.pow((a.getX()-b.getX()), 2)+Math.pow((a.getY()-b.getY()), 2));
   }
 
 
