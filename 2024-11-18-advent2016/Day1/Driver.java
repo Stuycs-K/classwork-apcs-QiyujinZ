9510b5b575e8daa2974f3269dd0d4a0473da613a
Qiyujin Zhang <qzhan7183@gmail.com>
2024-11-18 16:57:53 -0500

    Calculated the steps per direction. Next add how to find the coordinate

--- 2024-11-18-advent2016/Day1/Driver.java
+++ 2024-11-18-advent2016/Day1/Driver.java
@@ -8,11 +8,41 @@ public class Driver{
   
   public static int getDirections(String Filename){
     String directions = "";
+    int right = 0
+    int left = 0;
+    int north = 0;
+    int south = 0;
+    int east = 0;
+    int west = 0;
     try{
       File file = new File(Filename);
       Scanner input = new Scanner(file);
       while(input.hasNext()){
-        directions += input.next(); 
+        directions += input.next();
+        }
+      String Directions[] = directions.split(",");
+      for (int i = 0; i < Directions.length; i ++){
+        if(Directions[i].charAt(0) == 'R'){
+          direction += 90;
+          }
+        else{
+          direction -= 90;
+        }
+        
+        if (direction % 360 = 0){
+        north += (int) Directions[i].substring(1);
+        }
+        if (direction % 360 = 90){
+        east += (int) Directions[i].substring(1);
+        }
+        if (direction % 360 = 180){
+        south += (int) Directions[i].substring(1);
+        }
+        if (direction % 360 = 270){
+        west += (int) Directions[i].substring(1);
+        }
+        }
+        }
       }
       
       
