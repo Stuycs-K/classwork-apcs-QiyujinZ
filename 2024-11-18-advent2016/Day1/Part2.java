5346a6e8ba552b0deab7f855e27211efd5054dba
Qiyujin Zhang <qzhan7183@gmail.com>
2024-11-20 21:05:21 -0500

    Completed Part2

--- 2024-11-18-advent2016/Day1/Part2.java
+++ 2024-11-18-advent2016/Day1/Part2.java
@@ -2,6 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Part2{  
+
+  public static boolean Check(ArrayList<String> Coordinates,String point){
+	for (int j = 0; j < Coordinates.size(); j++){
+	System.out.println(Coordinates.get(j));
+        if (Coordinates.get(j).equals(point)){
+            return (true);  
+		}
+	}
+	return false;
+  }
+  
   public static int getDirections(String Filename){
     int direction = 0;
     String directions = "";
@@ -12,40 +23,64 @@ public class Part2{
     try{
       File file = new File(Filename);
       Scanner input = new Scanner(file);
+	  Coordinates.add("(0,0)");
+	  System.out.println(Coordinates.get(0));
       while(input.hasNext()){
         directions += input.next();
         }
       String Directions[] = directions.split(",");
       for (int i = 0; i < Directions.length; i ++){
         if(Directions[i].charAt(0) == 'R'){
-          direction += + 360 + 90;
+          direction += 360 + 90;
           }
         if(Directions[i].charAt(0) == 'L'){
           direction += 360 - 90;
         }
         
         if (Math.abs(direction) % 360 == 0){
-        vertical += Integer.parseInt(Directions[i].substring(1));
-        }
-        if (Math.abs(direction) % 360 == 90){
-        horizontal += Integer.parseInt(Directions[i].substring(1));
-        }
-        if (Math.abs(direction) % 360 == 180){
-        vertical -= Integer.parseInt(Directions[i].substring(1));
-        }
-        if (Math.abs(direction) % 360 == 270){
-        horizontal -= Integer.parseInt(Directions[i].substring(1));
-        }
-        point = "";
-        point = "("+horizontal+","+vertical+")";
-        System.out.println(point);
-        for (int j = 0; j < Coordinates.size(); i++){
-          if (Coordinates.get(j).equals(point)){
-            return (Math.abs(horizontal)+Math.abs(vertical));
+         for (int n = 0; n < Integer.parseInt(Directions[i].substring(1)); n++){
+		   vertical += 1;
+		   point = "("+horizontal+","+vertical+")";
+		   if (Check(Coordinates,point)){
+			   return (Math.abs(horizontal)+Math.abs(vertical));
+            }
+			Coordinates.add(point);
           }
         }
-        Coordinates.add(point);
-       }
+		
+        if (Math.abs(direction) % 360 == 90){
+         for (int n = 0; n < Integer.parseInt(Directions[i].substring(1)); n++){
+		   horizontal += 1;
+		   point = "("+horizontal+","+vertical+")";
+		   if (Check(Coordinates,point)){
+			   return (Math.abs(horizontal)+Math.abs(vertical));
+            }
+			Coordinates.add(point);
+          }
+		}
+		
+        if (Math.abs(direction) % 360 == 180){
+         for (int n = 0; n < Integer.parseInt(Directions[i].substring(1)); n++){
+		   vertical -= 1;
+		   point = "("+horizontal+","+vertical+")";
+		   if (Check(Coordinates,point)){
+			   return (Math.abs(horizontal)+Math.abs(vertical));
+            }
+			Coordinates.add(point);
+		 }
+		 }
+		 
+        if (Math.abs(direction) % 360 == 270){
+         for (int n = 0; n < Integer.parseInt(Directions[i].substring(1)); n++){
+		   horizontal -= 1;
+		   point = "("+horizontal+","+vertical+")";
+		   if (Check(Coordinates,point)){
+			   return (Math.abs(horizontal)+Math.abs(vertical));
+            }
+			Coordinates.add(point);
+          }
+        }
+	  }
       }
     catch(Exception e){
     
