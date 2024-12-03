6c704742ff45a1decfb0c5bca7f2aa9e6de04cec
Qiyujin Zhang <qzhan7183@gmail.com>
2024-12-03 17:00:49 -0500

    Made the red stripes

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,5 +1,5 @@
 public class ColorDemo{
-  
+  public static final String ESC = "\u001b[";
   public static final String CLEAR_SCREEN =  "\u001b[2J";
   public static final String HIDE_CURSOR =  "\u001b[?25l";
   public static final String SHOW_CURSOR =  "\u001b[?25h";
@@ -9,6 +9,10 @@ public class ColorDemo{
   }
     
   public static void main (String[] args){
-    
-  }
+    for (int i = 0; i < 10; i ++){
+        for (int j = 0; j < 256; j++){
+          System.out.print(ESC+"48;2;"+j+";"+i+";12m.");
+            }
+        }
+    }
 }
