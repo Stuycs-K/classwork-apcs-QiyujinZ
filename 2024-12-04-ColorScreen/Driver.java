256286e962fae615a8f91291a6abcf6cd04a650c
Qiyujin Zhang <qzhan7183@gmail.com>
2024-12-04 15:25:37 -0500

    add randomList written previously from lab08

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,7 +1,11 @@
+import java.util.*;
+import java.io.*;
+
 public class Driver extends Text{
 
     public static void drawBorder(int height, int width){
         for (int j = 0; j < height; j ++){
+            System.out.print("         ");
             for (int i = 0; i < width; i ++){
               if (j == 0 || j == height-1){
                 color(background(GREEN));
@@ -20,9 +24,22 @@ public class Driver extends Text{
         }
     }
     
+    public static void drawIntegers(int[] data){
+      
+    }
     
+    public static int[]randomList (int size){
+        int[] numList = new int[size];
+        for (int i = 0; i < size; i ++){
+            numList[i] = (int) (Math.random()*100);
+        } 
+        return numList;
+      }
+      
+      
     public static void main (String[] args){
-        drawBorder(30,100);
+        drawBorder(30,80);
+        int[] numbers = randomList(3);
         System.out.println(RESET);
   }
 }
