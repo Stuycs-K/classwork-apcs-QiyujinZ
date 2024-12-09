4a5a4a96b139a2c59e865e875122ccc798b1fe37
Qiyujin Zhang <qzhan7183@gmail.com>
2024-12-08 21:17:16 -0500

    Tested support (healing)

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -4,5 +4,6 @@ public class Driver{
     Caster Alpha = new Caster("Alpha");
     Caster Bravo = new Caster("Bravo",10,30);
 	System.out.println(Alpha.attack(Bravo));
+	System.out.println(Alpha.support(Bravo));
   }
 }
