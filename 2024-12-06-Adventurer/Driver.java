133b9e1c6361bc172fbf36fd0c39a966555f299e
Qiyujin Zhang <qzhan7183@gmail.com>
2024-12-08 21:14:25 -0500

    Tested Attack

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -2,6 +2,7 @@ public class Driver{
   
   public static void main(String[] args){
     Caster Alpha = new Caster("Alpha");
-    Caster Bravo = new Caster("Bravo",100);
+    Caster Bravo = new Caster("Bravo",10,30);
+	System.out.println(Alpha.attack(Bravo));
   }
 }
