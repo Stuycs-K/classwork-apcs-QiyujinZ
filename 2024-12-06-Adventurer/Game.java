b3253063cd6c768d97546a8e250da6305831ac05
Qiyujin Zhang <qzhan7183@gmail.com>
2024-12-09 16:09:52 -0500

    Completed constrution for player inputs

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -4,26 +4,40 @@ import java.io.*;
 public class Game{
   
   public static void main (String[] args){
-    int pAction, eAction = 0;
+    int pAction = 0;
+    int eAction = 0;
     CodeWarrior Enemy = new CodeWarrior ("Enemy");
     Caster Player = new Caster ("Player");
-    
     Scanner userInput = new Scanner (System.in);
     String input = userInput.nextLine();
     while (!(input.equals("quit"))){
       if(Player.getHP() == 0){
         System.out.println("The winner is" + Enemy.getName());
       }
-      if(Enemy.getHP() == 0){
+      else if(Enemy.getHP() == 0){
         System.out.println("The winner is" + Player.getName());
       }
       else{
-         if (pAction == eAction){
-           pAction += 1;
-           System.out.println(Player.getName() + ", it's your turn to action!");
-           
-         }
+        System.out.println(Player.getName() + ", HP: " + Player.getHP() + "/" + Player.getmaxHP() + ", mana: " + Player.getSpecial() + "/" + Player.getSpecialMax());
+        System.out.println(Enemy.getName() + ", HP: " + Enemy.getHP() + "/" + Enemy.getmaxHP() + ", mana: " + Enemy.getSpecial() + "/" + Enemy.getSpecialMax());
+        if (pAction == eAction){
+          pAction += 1;
+          System.out.println(Player.getName() + ", it's your turn to action!");
+          if (input.equals("a")|| input.equals("attack")){
+            Player.attack(Enemy);
+            }
+          else if (input.equals("sp")||input.equals("special")){
+            Player.specialAttack(Enemy);
+          }
+          else if (input.equals("su")||input.equals("support")){
+            Player.support();
+          }
+          }
+        else if (eAction < pAction){
+          eAction += 1;
+          System.out.println(Enemy.getName() + ", it's your turn to action!");
+          }
+        }
       }
     }
     }
-  }
