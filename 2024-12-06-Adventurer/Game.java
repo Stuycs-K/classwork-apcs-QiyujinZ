bd9979ba43fbc351272cea13ce31cf8718fe6b98
Qiyujin Zhang <qzhan7183@gmail.com>
2024-12-09 16:21:41 -0500

    Added System dialogue to print when the option isn't valid

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -10,7 +10,7 @@ public class Game{
     Caster Player = new Caster ("Player");
     Scanner userInput = new Scanner (System.in);
     String input = userInput.nextLine();
-    System.out.println("Hello! This is a fun RPG. Your mission is to beat your enemy. type 'a' or 'attack' to attack. type 'sp' or 'special' to do special Attack. type 'su' or 'support' to recover HP and mana.");
+    System.out.println("Hello! This is a fun RPG. Your mission is to beat your enemy. type 'a' or 'attack' to attack. type 'sp' or 'special' to do special Attack. type 'su' or 'support' to recover HP and mana. type 'quit' to quit. ");
     while (!(input.equals("quit"))){
       if(Player.getHP() == 0){
         System.out.println("The winner is" + Enemy.getName());
@@ -33,11 +33,23 @@ public class Game{
           else if (input.equals("su")||input.equals("support")){
             System.out.println(Player.support());
           }
+          else{
+            System.out.println("That is not a valid option! As a reminder: type 'a' or 'attack' to attack. type 'sp' or 'special' to do special Attack. type 'su' or 'support' to recover HP and mana. type 'quit' to quit. ");
+          }
           }
         else if (eAction < pAction){
           eAction += 1;
           System.out.println(Enemy.getName() + ", it's your turn to action!");
-            
+          int option = (int) Math.random()*3;
+            if (option == 1){
+              System.out.println(Enemy.attack(Player));
+            }
+            else if (option == 2){
+              System.out.println(Player.specialAttack(Enemy));
+            }
+            else if (option == 0){
+              System.out.println(Player.support());
+            }
           }
         }
       }
