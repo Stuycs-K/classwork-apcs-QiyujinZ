7e0f61183a6d5dd1f16dcafd6a048d9b3e223096
Qiyujin Zhang <qzhan7183@gmail.com>
2025-01-03 10:43:38 -0600

    fixed args index

--- 2025-01-02/makeWords.java
+++ 2025-01-02/makeWords.java
@@ -4,13 +4,13 @@ public class makeWords{
     System.out.println(result);
     if(remainingLetters > 0){
       for (int i = 0; i < alphabet.length(); i ++){
-        makeWords(remaingingLetters-1,result+alphabet.substring(i,i+1),alphabet);
+        makeWords(remainingLetters-1,result+alphabet.substring(i,i+1),alphabet);
       }
     }
   }
   
   
   public static void main(String[] args){
-    makeWords(args[1],args[2],args[3]);
+    makeWords(Integer.valueOf(args[0]),args[1],args[2]);
   }
 }
